cb6f8ebf89041070566179283e9e5bceb9cb9363
Aryaputra Haidar Akbar <77577517+aryaptrha@users.noreply.github.com>
2023-05-31 15:05:49 +0700

    adding more user management test and minor change on user_page and add user

---
+++ user_management/delete_user.cy.js
@@ -0,0 +1,58 @@
+describe('Delete User', () => {
+  it('should successfully delete user', () => {
+
+    // Handle any errors
+    cy.on('uncaught:exception', (err, runnable) => {
+      // Cypress will fail the test if an uncaught exception occurs, so return false to prevent that
+      return false
+    })
+
+    //Login
+    cy.visit('https://opensource-demo.orangehrmlive.com/')
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+    cy.get('.oxd-button').click()
+
+    //Click Admin
+    cy.get(':nth-child(1) > .oxd-main-menu-item').click()
+
+    //Assert that successfully directed to users page
+    cy.url().should('include', '/admin/viewSystemUsers')
+
+    // Click on the "Delete" button for the first user
+    cy.get(':nth-child(4) > .oxd-table-row > :nth-child(6) > .oxd-table-cell-actions > :nth-child(1)').click({ force: true })
+
+    //Confirm Delete
+    cy.get('.oxd-button--label-danger').click()
+
+    // Assert that user was edited successfully
+    cy.contains('Successfully Deleted').should('be.visible')
+  })
+
+  it('should successfully cancel delete user', () => {
+
+    // Handle any errors
+    cy.on('uncaught:exception', (err, runnable) => {
+      // Cypress will fail the test if an uncaught exception occurs, so return false to prevent that
+      return false
+    })
+
+    //Login
+    cy.visit('https://opensource-demo.orangehrmlive.com/')
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+    cy.get('.oxd-button').click()
+
+    //Click Admin
+    cy.get(':nth-child(1) > .oxd-main-menu-item').click()
+
+    //Assert that successfully directed to users page
+    cy.url().should('include', '/admin/viewSystemUsers')
+
+    //Click on the "Delete" button for the first user
+    cy.get(':nth-child(4) > .oxd-table-row > :nth-child(6) > .oxd-table-cell-actions > :nth-child(1)').click({ force: true })
+
+    //Cancel Delete
+    cy.get('.oxd-button--text').click()
+  })
+})
--- user_management/edit_user.cy.js
+++ user_management/edit_user.cy.js
@@ -1,12 +1,15 @@
-describe('User Page', () => {
-
-  it('should directed to users page', () => {
+describe('Edit User', () => {
+  it('should successfully edit user', () => {
 
     // Handle any errors
     cy.on('uncaught:exception', (err, runnable) => {
       // Cypress will fail the test if an uncaught exception occurs, so return false to prevent that
       return false
     })
+
+    const uuid = () => Cypress._.random(0, 1e6)
+    const id = uuid()
+    const testname = `testname${id}`
 
     //Login
     cy.visit('https://opensource-demo.orangehrmlive.com/')
@@ -19,5 +22,15 @@ describe('User Page', () => {
 
     //Assert that successfully directed to users page
     cy.url().should('include', '/admin/viewSystemUsers')
+
+    // Click on the "Edit" button for the first user
+    cy.get(':nth-child(1) > .oxd-table-row > :nth-child(6) > .oxd-table-cell-actions > :nth-child(2)').click({ force: true })
+
+    // Edit user details
+    cy.get(':nth-child(2) > .oxd-input').clear().type(testname)
+    cy.get('.oxd-button--secondary').click()
+
+    // Assert that user was edited successfully
+    cy.contains('Successfully Updated').should('be.visible')
   })
 })
